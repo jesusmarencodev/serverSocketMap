bb714d7716b384a4954b5d96bc0d896405681cf3
Jesus Marenco <jesusmarencodev@gmail.com>
2021-04-20 17:44:12 -0500

    agregando sockets

--- model/sockets.js
+++ model/sockets.js
@@ -1,16 +1,30 @@
+const Marcadores = require("./marcadores");
+
 class Sockets {
   constructor(io){
     this.io = io;
+    this.marcadores = new Marcadores();
     this.socketEvents();
   }
 //comentario
   socketEvents(){
     //On connection
     this.io.on('connection', ( socket ) => {
-      socket.on('mensaje-to-server', (data)=>{
-        console.log(data);
-        this.io.emit('server-to-client', {data});
+
+      //Todo: cuando alguien se conecte enviamos marcadores activos
+
+      socket.emit('marcadores-activos', this.marcadores.activos);
+
+      //Todo marcador-nuevo
+
+      socket.on('marcador-nuevo', (marcador)=>{
+        this.marcadores.agregarMarcador(marcador);
+
+        socket.broadcast.emit('marcador-nuevo', marcador);
       });
+
+
+      //Todo:marcador actualizado
     });
     
   }
